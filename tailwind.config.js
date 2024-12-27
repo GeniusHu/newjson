baf867a2d053e27d9cb9949efd684f2b67d9bf94
huguanhu <huguanhu@qimao.com>
2024-12-27 23:13:42 +0800

    fix vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+      animation: {
+        'gradient': 'gradient 8s linear infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
